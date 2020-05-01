31f4d56c331bcfd4a4d8dd8bf5d96c734a4e9889
ezhvsalate <ezhvsalate@ya.ru>
2020-05-01 08:32:03 +0300

    Updates

---
+++ precache-manifest.3d165e71a29a0077d040e46652596845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f29f0c9c1ec39f27465808f146fdeb",
+    "url": "/react_training/index.html"
+  },
+  {
+    "revision": "a4853fb453be169c1fee",
+    "url": "/react_training/static/css/main.98836e6a.chunk.css"
+  },
+  {
+    "revision": "8982cf8337dcd338ef8a",
+    "url": "/react_training/static/js/2.202d88f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_training/static/js/2.202d88f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4853fb453be169c1fee",
+    "url": "/react_training/static/js/main.d225d73a.chunk.js"
+  },
+  {
+    "revision": "708a0b03903d81bea6ff",
+    "url": "/react_training/static/js/runtime-main.7a4fb846.js"
+  }
+]);
